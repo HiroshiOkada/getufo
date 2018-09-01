ae82f88e8c7e39dddfe3179b2dcf562475a6346d
Hiroshi Okada <okadahiroshi@miobox.jp>
2018-09-01 21:58:26 +0900

    Update working directory before fetch sec_to_edinet.json

--- _update_sec_to_edinet.sh
+++ _update_sec_to_edinet.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+git pull --ff-only
 curl --remote-name https://gist.githubusercontent.com/HiroshiOkada/5a2ac826530cc851a897f739f36618f7/raw/sec_to_edinet.json
 
 change=$(git status --porcelain)
